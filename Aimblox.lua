5fb56b212a040d4c6bd6552242a833fb47a6ccc2
NotVX <65828328+NotVX@users.noreply.github.com>
2021-07-16 22:37:51 +0200

    uhhh i强无敌抢我们的去名气我们的iq我们的期末的气味的吗i吗

---
+++ Aimblox.lua
@@ -0,0 +1,25 @@
+for i, v in pairs(getgc(true)) do
+    if type(v) == "table" then
+        if rawget(v, "MaxAmmo") and type(rawget(v, "MaxAmmo")) == "number" then
+            v.MaxAmmo = math.huge
+            v.HorizontalRecoil = 0
+            v.VerticalRecoil = 0
+        end
+        if rawget(v, "Ammo") and type(rawget(v, "Ammo")) == "number" then
+            v.Ammo = math.huge
+            v.DropSpread = 0
+            v.SpreadStep = 0
+            v.MinSpread = 0
+            v.MaxSpread = 0
+            v.GunFireStopStateDelay = 0
+            v.ReloadTime = 0
+            v.GunEquipDelay = 0
+        end
+        if rawget(v, "Recoil") and type(rawget(v, "Recoil")) == "number" then
+            v.RPM = 10000
+            v.VerticalClimbDrop = 0
+            v.Range = 10000
+            v.Recoil = 0
+        end
+    end
+end
